569380d4d70569e16e49a92a064f6f303d430bac
Ahmad Rassya Maulana <rrassya19@gmail.com>
2026-07-29 10:02:56 +0700

    Add authorization token validation in authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
     try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader ) {
+            return res.status(401).json({ 
+                message: 'Authorization token tidak ditemukan' 
+            });
+        }
 
     } catch (error) {
         return res.status(401).json({
