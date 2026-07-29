19f997e95f132b3f0d13474aee3dfdef09608dd5
Ahmad Rassya Maulana <rrassya19@gmail.com>
2026-07-29 11:00:31 +0700

    Add genre model to define Genre entity with id and name attributes

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Genre = sequelize.define('Genre', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    return Genre;
+};
