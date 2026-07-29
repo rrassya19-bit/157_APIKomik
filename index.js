0751b27eadab21a6de38a779054c6d02afd28841
Ahmad Rassya Maulana <rrassya19@gmail.com>
2026-07-29 10:17:04 +0700

    Add API route handling in index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", require("./routes/api"));
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
