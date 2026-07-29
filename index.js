e66270108edf9ba89884fff806d206f37c007854
Ahmad Rassya Maulana <rrassya19@gmail.com>
2026-07-29 10:16:51 +0700

    Add middleware for JSON and URL-encoded request parsing

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
